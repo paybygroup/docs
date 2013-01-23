5aeca9ac0117319b374d0930d8049adecde3ffa0
Dan Oblinger <github@oblinger.us>
2013-01-23 15:04:57 -0800

    docs

--- app.rb
+++ app.rb
@@ -20,7 +20,7 @@ end
 get '/:article' do
   @content = RDiscount.new( File.open("contents/" + params["article"].gsub("-", "_").concat(".md")).read ).to_html
   doc_title = Nokogiri::HTML::DocumentFragment.parse( @content ).css('h1').inner_html()  
-  @title = "#{doc_title} | Observaciones de un explorador, por César Salazar"
+  @title = "#{doc_title} | PayByGroup Documentation"
   haml :article
 end
   
