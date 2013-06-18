adc9a8b6242ecf5a840ab06e26d3fae500b4a211
Federico Soria <fede@Fede-Airness.local>
2013-06-18 16:03:47 -0700

    Cache response from status

--- app.rb
+++ app.rb
@@ -11,23 +11,26 @@ require 'pry'
 require 'pingdom-client'
 
 @@status = get_status
+@@last_check_status = Time.current
 
 use Rack::Auth::Basic, "Restricted Area" do |username, password|
   [username, password] == ['merchant', 'docs']
 end
 
 def get_status
-  unless defined? @@client
-    @@client = Pingdom::Client.new(
-      :username => 'webmaster@paybygroup.com', 
-      :password => 'newcrew11', 
-      :key => 'ynla346encc9zl3ou0rsdxd3d5uzylrx')
+  if @@last_check_status > Time.current - 30.minutes
+    unless defined? @@client
+      @@client = Pingdom::Client.new(
+        :username => 'webmaster@paybygroup.com', 
+        :password => 'newcrew11', 
+        :key => 'ynla346encc9zl3ou0rsdxd3d5uzylrx')
+    end
+    @@status = @@client.test!(:host => "lets.paybygroup.com", :type => "http").status
+    @@last_check_status = Time.current
   end
-    @@client.test!(:host => "lets.paybygroup.com", :type => "http").status
 end
 
 get '/' do
-  @@status = get_status
   client = Pingdom::Client.new(
     :username => 'webmaster@paybygroup.com', 
     :password => 'newcrew11', 
@@ -37,7 +40,7 @@ get '/' do
 end
 
 get '/:article' do
-  @@status = get_status
+
   @content = RDiscount.new( File.open("contents/" + params["article"].gsub("-", "_").concat(".md")).read ).to_html
   doc_title = Nokogiri::HTML::DocumentFragment.parse( @content ).css('h1').first.inner_html()
   @sidebar = Nokogiri::HTML::DocumentFragment.parse( @content ).css('h4')
